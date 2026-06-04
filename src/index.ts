3dbc9e6292234fd37ff9c7dd896516253ec4286f
Svetozar Stoev <zarkoto23@gmail.com>
2026-06-04 22:38:14 +0300

    some refactoring and add cors module

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,53 @@
 import express from "express";
-import path from "node:path";
+import { type Request, type Response } from "express";
+import fs from "fs";
+import path from "path";
+import cors from "cors";
 
 const app = express();
 
-const videos = [
-  {
-    id: "knives_out",
-    title: "Knives Out (2019)",
-    playlist: "http://localhost:8080/hls/knives_out_1080/index.m3u8"
-  },
-  {
-    id: "curse_black_pearl", 
-    title: "Pirates of the Caribbean",
-    playlist: "http://localhost:8080/hls/curse_black_pearl/index.m3u8"
+app.use(cors());
+
+const VIDEOS_PATH = "D:/SoftUni/strm-project/videos";
+
+function getVideos() {
+  try {
+    const folders = fs.readdirSync(VIDEOS_PATH);
+    const videos = [];
+
+    for (const folder of folders) {
+      const m3u8Path = path.join(VIDEOS_PATH, folder, "index.m3u8");
+      if (fs.existsSync(m3u8Path)) {
+        videos.push({
+          id: folder,
+          title: folder.replace(/_/g, " ").replace(/-/g, " "),
+          playlist: `http://localhost:8080/hls/${folder}/index.m3u8`,
+        });
+      }
+    }
+    return videos;
+  } catch (err) {
+    console.log("грешка при четене на папка с видеа", err);
+    return [];
   }
-];
+}
 
+app.get("/videos", (req: Request, res: Response) => {
+  const videos = getVideos();
+  res.json(videos);
+});
 
-app.get("/videos",(req, res )=>{
-  res.json(videos)
-})
+app.get("/videos/:id", (req: Request, res: Response) => {
+  const videos = getVideos();
+  const video = videos.find((v) => v.id === req.params.id);
 
-
-app.get("/videos/:id",(req, res)=>{
-  const video=videos.find(v=>v.id===req.params.id)
-  if(!video){
-    res.status(404).json({message:"Not Fount"})
-    return
+  if (!video) {
+    return res.status(404).json({ message: "not found" });
   }
-
-  res.json(video)
-})
-
+  res.json(video);
+});
 
 const PORT = 3000;
-
 app.listen(PORT, () => {
-  console.log(`Servers is running on: http://localhost:${PORT}`);
+  console.log(`Node.js API running on: http://localhost:${PORT}`);
 });
