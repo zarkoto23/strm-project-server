4eb6a831bcb342674ed8e619e9485b7b4c8756d8
Svetozar Stoev <zarkoto23@gmail.com>
2026-06-05 17:08:45 +0300

    refactor directories location and files names

--- src/index.ts
+++ src/index.ts
@@ -8,43 +8,43 @@ const app = express();
 
 app.use(cors());
 
-const VIDEOS_PATH = "D:/SoftUni/strm-project/videos";
+const MOVIES_PATH = "E:\movies-m3u8";
 
-function getVideos() {
+function getMovies() {
   try {
-    const folders = fs.readdirSync(VIDEOS_PATH);
-    const videos = [];
+    const folders = fs.readdirSync(MOVIES_PATH);
+    const movies = [];
 
     for (const folder of folders) {
-      const m3u8Path = path.join(VIDEOS_PATH, folder, "index.m3u8");
+      const m3u8Path = path.join(MOVIES_PATH, folder, "index.m3u8");
       if (fs.existsSync(m3u8Path)) {
-        videos.push({
+        movies.push({
           id: folder,
-          title: folder.replace(/_/g, " ").replace(/-/g, " "),
+          title: folder,
           playlist: `http://localhost:8080/hls/${folder}/index.m3u8`,
         });
       }
     }
-    return videos;
+    return movies;
   } catch (err) {
     console.log("грешка при четене на папка с видеа", err);
     return [];
   }
 }
 
-app.get("/videos", (req: Request, res: Response) => {
-  const videos = getVideos();
-  res.json(videos);
+app.get("/movies", (req: Request, res: Response) => {
+  const movies = getMovies();
+  res.json(movies);
 });
 
-app.get("/videos/:id", (req: Request, res: Response) => {
-  const videos = getVideos();
-  const video = videos.find((v) => v.id === req.params.id);
+app.get("/movies/:id", (req: Request, res: Response) => {
+  const movies = getMovies();
+  const movie = movies.find((v) => v.id === req.params.id);
 
-  if (!video) {
+  if (!movie) {
     return res.status(404).json({ message: "not found" });
   }
-  res.json(video);
+  res.json(movie);
 });
 
 const PORT = 3000;
